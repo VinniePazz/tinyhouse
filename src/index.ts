5f53fdb9c6ca7e148a665c5d88519662c58f985b
dimas <dimasmakouz@gmail.com>
2020-04-20 19:49:57 +0300

    mock listings functionallity

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { listings } from './listings';
+
 const app = express();
 const port = 9000;
 
-app.get('/', (_req, res) => res.send('hello world'));
-app.listen(port);
+app.use(bodyParser.json());
 
-console.log(`[app] : http://localhost:${port}`);
+app.get('/', (_req, res) => res.send('hello world'));
+
+// get all listings
+app.get('/listings', (_req, res) => res.send(listings));
+
+//delete listings
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send('failed to delete listing');
+});
+
+app.listen(port, () => {
+  console.log(`[app] : http://localhost:${port}`);
+});
