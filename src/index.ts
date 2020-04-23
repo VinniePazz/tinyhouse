04ef17f2bc020177b237baa35c2e8260b1f39a1b
dimas <dimasmakouz@gmail.com>
2020-04-23 16:32:08 +0300

    graphql initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
+
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
+
 const port = 9000;
-
-app.use(bodyParser.json());
-
-app.get('/', (_req, res) => res.send('hello world'));
-
-// get all listings
-app.get('/listings', (_req, res) => res.send(listings));
-
-//delete listings
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send('failed to delete listing');
-});
 
 app.listen(port, () => {
   console.log(`[app] : http://localhost:${port}`);
