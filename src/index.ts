1814b153128df40882c399f712b9a7244afccd2c
dimas <dimasmakouz@gmail.com>
2020-04-25 15:05:55 +0300

    added mongoDB Atlas setup and env

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import { typeDefs, resolvers } from './graphql/';
+import { connectDatabase } from './database';
+import { typeDefs, resolvers } from './graphql';
 
 const app = express();
-
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
-
 const port = 9000;
 
-app.listen(port, () => {
-  console.log(`[app] : http://localhost:${port}`);
-});
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+  server.applyMiddleware({ app, path: '/api' });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`[app] : http://localhost:${port}`);
+  });
+
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+};
+
+mount(app);
