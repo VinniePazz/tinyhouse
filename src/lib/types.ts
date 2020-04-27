9b0f839fba33a64ca07c5db7a33f5df9df8b0be3
dimas <dimasmakouz@gmail.com>
2020-04-28 00:03:19 +0300

    Add db document structure

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,57 @@
 import { ObjectId, Collection } from 'mongodb';
 
-export interface Listing {
-  _id: ObjectId;
+enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE',
+}
+
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
 }
 
 export interface User {
+  _id: string; // string instead of ObjectId for third-party auth libs
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[]; // one-to-many relationship
+  listings: ObjectId[]; // one-to-many relationship
+}
+
+export interface Listing {
   _id: ObjectId;
+  title: string;
+  description: string;
+  image: string;
+  host: string; // one-to-one relationship (User id which is a string)
+  type: ListingType;
+  country: string;
+  city: string;
+  address: string;
+  admin: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  price: number;
+  numOfGuests: number;
 }
 
 export interface Booking {
   _id: ObjectId;
+  listing: ObjectId; // one-to-one
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
 
 export interface Database {
